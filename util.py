bc1beb03db2da97cea6f5d621d9b9b4ed153e53d
Thunthup Kiratisehwe <thunthup123@gmail.com>
2022-02-04 16:24:58 +0700

    edit yRatio

--- util.py
+++ util.py
@@ -1,56 +1,64 @@
 import math
 import cv2
 import ThresholdValue
-def calDist(lm1,lm2):
-    #calculate the distance of (x1,y1,z1) and (x2,y2,z2) from landmarks
+import copy
+
+
+def calDist(lm1, lm2):
+    # calculate the distance of (x1,y1,z1) and (x2,y2,z2) from landmarks
     return math.sqrt((lm1.x-lm2.x)**2+(lm1.y-lm2.y)**2+(lm1.z-lm2.z)**2)
+
+
+def calDistY(lm1, lm2):
+
+    return lm1.y-lm2.y
+
 
 def getXRatio(face_landmarks):
     leftIris = face_landmarks.landmark[468]
     insideLeft = face_landmarks.landmark[133]
     outsideLeft = face_landmarks.landmark[33]
     # calculate ratio of distance between iris and outside eye corner and inside eye corner
-    xLeftRatio = (calDist(leftIris,insideLeft)/calDist(leftIris,outsideLeft)*100)
+    xLeftRatio = (calDist(leftIris, insideLeft) /
+                  calDist(leftIris, outsideLeft)*100)
     rightIris = face_landmarks.landmark[473]
     insideRight = face_landmarks.landmark[362]
     outsideRight = face_landmarks.landmark[263]
-    xRightRatio = (calDist(rightIris,outsideRight)/calDist(rightIris,insideRight)*100)
-    #find the average of both eyes
-    xRatio = (xLeftRatio + xRightRatio)/2 
+    xRightRatio = (calDist(rightIris, outsideRight) /
+                   calDist(rightIris, insideRight)*100)
+    # find the average of both eyes
+    xRatio = (xLeftRatio + xRightRatio)/2
     return xRatio
 
-def getYRatio(face_landmarks,xText):
-    leftIris = face_landmarks.landmark[468]
-    insideLeft = face_landmarks.landmark[133]
-    outsideLeft = face_landmarks.landmark[33]
-    lowerLeft1 = face_landmarks.landmark[163]
-    lowerLeft2 = face_landmarks.landmark[144]
-    lowerLeft3 = face_landmarks.landmark[145]
-    lowerLeft4 = face_landmarks.landmark[153]
-    lowerLeft5 = face_landmarks.landmark[154]
-    
-    leftEyeSize = calDist(insideLeft,outsideLeft)
-    
-    if xText == "left":
-        leftLowerDist = (calDist(leftIris,lowerLeft1) + calDist(leftIris,lowerLeft2)/leftEyeSize)
-    elif xText == "mid":
-        leftLowerDist = calDist(leftIris,lowerLeft3)/leftEyeSize 
-    else:
-        leftLowerDist = (calDist(leftIris,lowerLeft4) + calDist(leftIris,lowerLeft5))/leftEyeSize
-    
-    yLeft = leftLowerDist*100
-#     rightIris = face_landmarks.landmark[477]
-#     upperRight = face_landmarks.landmark[362]
-#     lowerRight = face_landmarks.landmark[263]
-#     yRightRatio = math.log(calDist(rightIris,outsideRight)/calDist(rightIris,insideRight)*1000)
-#     yRatio = (yLeftRatio + yRightRatio)/2
-    return yLeft
+# def getYRatio(face_landmarks,xText):
+#     leftIris = face_landmarks.landmark[468]
+#     insideLeft = face_landmarks.landmark[133]
+#     outsideLeft = face_landmarks.landmark[33]
+#     lowerLeft1 = face_landmarks.landmark[163]
+#     lowerLeft2 = face_landmarks.landmark[144]
+#     lowerLeft3 = face_landmarks.landmark[145]
+#     lowerLeft4 = face_landmarks.landmark[153]
+#     lowerLeft5 = face_landmarks.landmark[154]
+#
+#     leftEyeSize = calDist(insideLeft,outsideLeft)
+#
+#     if xText == "left":
+#         leftLowerDist = (calDist(leftIris,lowerLeft1) + calDist(leftIris,lowerLeft2)/leftEyeSize)
+#     elif xText == "mid":
+#         leftLowerDist = calDist(leftIris,lowerLeft3)/leftEyeSize
+#     else:
+#         leftLowerDist = (calDist(leftIris,lowerLeft4) + calDist(leftIris,lowerLeft5))/leftEyeSize
+#
+#     yLeft = leftLowerDist*100
+#     return yLeft
 
-def getYTiltRatio(face_landmarks):        
+
+def getYTiltRatio(face_landmarks):
     forehead = face_landmarks.landmark[10]
     chin = face_landmarks.landmark[152]
     yTiltRatio = forehead.z-chin.z
     return yTiltRatio
+
 
 def getXTiltRatio(face_landmarks):
     leftCheek = face_landmarks.landmark[234]
@@ -59,3 +67,46 @@ def getXTiltRatio(face_landmarks):
     return xTiltRatio
 
 
+def getYRatio(face_landmarks):
+    leftIris = face_landmarks.landmark[468]
+    insideLeft = face_landmarks.landmark[133]
+    outsideLeft = face_landmarks.landmark[33]
+    leftEyeSize = calDist(insideLeft, outsideLeft)
+    yRatio = calDist(leftIris, face_landmarks.landmark[7])\
+        + calDist(leftIris, face_landmarks.landmark[163]) \
+        + calDist(leftIris, face_landmarks.landmark[154])\
+        + calDist(leftIris, face_landmarks.landmark[155])
+    return yRatio/leftEyeSize
+
+
+def getYRatio2(face_landmarks):
+    leftIris = face_landmarks.landmark[468]
+    insideLeft = face_landmarks.landmark[133]
+    outsideLeft = face_landmarks.landmark[33]
+    leftEyeSize = calDist(insideLeft, outsideLeft)
+    yRatio = calDist(leftIris, face_landmarks.landmark[145])
+
+    return yRatio/leftEyeSize
+
+
+def getSectionFromXY(X, Y, div, width=1920, height=1080):
+    xBorders = [(0 + width*e//div) for e in range(div)]
+    yBorders = [(0 + height*e//div) for e in range(div)]
+    xSection = 0
+    ySection = 0
+    for idx in range(div-1, -1, -1):
+        if X > xBorders[idx]:
+            xSection = idx
+            break
+    for idx in range(div-1, -1, -1):
+        if Y > yBorders[idx]:
+            ySection = idx
+            break
+    return (xSection, ySection)
+
+
+def getMousePosFromSection(sections, div, width=1920, height=1080):
+    xSection, ySection = sections
+    xPos = int(width*(xSection+0.5)/div)
+    yPos = int(height*(ySection+0.5)/div)
+    return (xPos, yPos)
